374063fb294a7802dd68624116f5ad5a512558b4
Soumyajit Manna <122715874+soumyajit-manna@users.noreply.github.com>
2025-02-01 10:54:35 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,6 +37,11 @@ const Navbar = () => {
           <li className="py-1">CONTACT</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
+
+        <a href="https://prescripto-admin-theta.vercel.app" className='bg-blue-100 border border-blue-500 px-5 text-xs py-1 rounded-full m-auto'>
+          <p className='mt-1'>ADMIN PANEL</p>
+        </a>
+        
       </ul>
       <div className="flex items-center gap-4">
         {
@@ -69,6 +74,7 @@ const Navbar = () => {
             <NavLink onClick={() => setShowMenu(false)} to='/doctors'><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
             <NavLink onClick={() => setShowMenu(false)} to='/about'><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
             <NavLink onClick={() => setShowMenu(false)} to='/contact'><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+            <a href="https://prescripto-admin-theta.vercel.app" className='px-4 py-2 rounded inline-block'>ADMIN PANEL</a>
           </ul>
         </div>
       </div>
